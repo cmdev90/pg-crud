de0af6f01a291f6df601d85a373bd7cfd43eddd0
Cherlton Millette <cherlton.millette@gmail.com>
2015-11-09 12:15:33 -0400

    Added isReady to the simple test script.

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ var pg = require('./');
 
 console.log(pg);
 
-pg.connect("postgres://postgres:hidden@localhost/sampledb");
+pg.connect("postgres://postgres:hidden@localhost:5433/sampledb");
 
 // wait 2 seconds to confirm the connection and initilize the object.
 setTimeout(function () {
-  pg.create('person', {name: 'Kalidia Millette', position: 'Cow Mechanic', rank: '...'}, function (err, result){
+	if (pg.isReady()){
+  	pg.create('person', {name: 'Kalidia Millette', position: 'Cow Mechanic', rank: '...'}, function (err, result){
       console.log(err, result); 
       // var recent = result.insertedIds[0];
       pg.retrieve('person', {name: 'Kalidia Millette'}, function (err, result) {
@@ -24,5 +25,9 @@ setTimeout(function () {
           });
         })
       });
-  });
+  	});
+	}
+	else {
+		console.log('Database not ready');
+	}
 }, 2000);
